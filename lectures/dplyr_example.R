b5a907112729f860b13b320bca916fd4b27b564e
Elizabeth Stregger <estregger@mta.ca>
2024-09-26 10:43:44 -0300

    update messy code

--- lectures/dplyr_example.R
+++ lectures/dplyr_example.R
@@ -9,3 +9,10 @@ flights_gain <- flights |>
 flights |> 
   select(where(is.character),
          tail_num =tailnum)
+
+arrange(mutate(filter(flights, dest == "DSM"), speed = distance / air_time * 60), desc(speed))
+
+flights_avg_delay <- flights |> 
+  group_by(dest) |> 
+  summarize(avg_delay = mean(dep_delay, na.rm = TRUE))
+View(flights_avg_delay)
