98946cfc18c409a6d47719547851efa59c30742a
Elizabeth Stregger <estregger@mta.ca>
2024-09-26 10:47:02 -0300

    update using styler

--- lectures/dplyr_example.R
+++ lectures/dplyr_example.R
@@ -1,18 +1,19 @@
 library(tidyverse)
 library(nycflights13)
 
-flights_gain <- flights |> 
+flights_gain <- flights |>
   mutate(
     gain = dep_delay - arr_delay
   )
 
-flights |> 
+flights |>
   select(where(is.character),
-         tail_num =tailnum)
+    tail_num = tailnum
+  )
 
 arrange(mutate(filter(flights, dest == "DSM"), speed = distance / air_time * 60), desc(speed))
 
-flights_avg_delay <- flights |> 
-  group_by(dest) |> 
+flights_avg_delay <- flights |>
+  group_by(dest) |>
   summarize(avg_delay = mean(dep_delay, na.rm = TRUE))
 View(flights_avg_delay)
